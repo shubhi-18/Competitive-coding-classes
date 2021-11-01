bb874bad208842259f3de26a6f388cb1974a09cf
shubhi-18 <60708059+shubhi-18@users.noreply.github.com>
2021-11-01 20:41:15 +0530

    Create hola.java

---
+++ hola.java
@@ -0,0 +1,23 @@
+package hola;
+
+public class hola {
+	private static void yo()
+	{
+		System.out.println("YO bro!!");
+		return;
+	}
+	private static void hello() {
+		System.out.println("Aur kya haal chaal hai?");
+		yo();
+		System.out.println("mast hai bahi");
+		return;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello();
+		System.out.println("mai ho main?");
+
+	}
+
+}
